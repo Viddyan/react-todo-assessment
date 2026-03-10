729dd18afa730bf96117529c4afc97de00ebdf28
viddyan <viddyanofficial@gmail.com>
2026-03-10 10:41:36 +0530

    add more tests

--- tests/todo.test.js
+++ tests/todo.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import React from "react";
 import App from "../src/App";
 
@@ -6,4 +6,31 @@ test("renders Todo heading", () => {
   render(<App />);
   const heading = screen.getByText("Todo App");
   expect(heading).toBeInTheDocument();
+});
+
+test("adds new todo", () => {
+  render(<App />);
+
+  const input = screen.getByPlaceholderText("Add todo");
+  const button = screen.getByText("Add");
+
+  fireEvent.change(input, { target: { value: "Learn React" } });
+  fireEvent.click(button);
+
+  expect(screen.getByText("Learn React")).toBeInTheDocument();
+});
+
+test("deletes todo", () => {
+  render(<App />);
+
+  const input = screen.getByPlaceholderText("Add todo");
+  const button = screen.getByText("Add");
+
+  fireEvent.change(input, { target: { value: "Learn React" } });
+  fireEvent.click(button);
+
+  const deleteBtn = screen.getByText("Delete");
+  fireEvent.click(deleteBtn);
+
+  expect(screen.queryByText("Learn React")).not.toBeInTheDocument();
 });
