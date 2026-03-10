a04fcd0227022a05428722ac0632aef3e90844d1
viddyan <viddyanofficial@gmail.com>
2026-03-10 11:05:32 +0530

    solution-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function App() {
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addTodo = () => {
+    if (!input) return;
+
+    setTodos([...todos, input]);
+    setInput("");
+  };
+
+  const deleteTodo = (index) => {
+    const updated = todos.filter((_, i) => i !== index);
+    setTodos(updated);
+  };
+
   return (
     <div>
       <h1>Todo App</h1>
+
+      <input
+        placeholder="Add todo"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+
+      <button onClick={addTodo}>Add</button>
+
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
+
+export default App;
